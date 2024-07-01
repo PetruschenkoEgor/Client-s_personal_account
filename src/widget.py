232b38630e59f0a0970ee493792bf5a7d7d3847d
Egor Petruschenko <jaguar.94@mail.ru>
2024-07-01 11:59:26 +0600

    created a function that distinguishes between a card or an account and makes a mask

---
+++ src/widget.py
@@ -0,0 +1,26 @@
+import masks
+
+
+def mask_account_card(number_card_or_account: str) -> str:
+    """Функция принимает номер карты/счета и возвращает маску номера"""
+    # Получаем из входной строки номер карты или счета(int)
+    number_lst = []
+    letter_lst = []
+    for i in number_card_or_account:
+        if i.isdigit():
+            number_lst.append(i)
+        else:
+            letter_lst.append(i)
+
+    # Используется эта маска, если принимаемый аргумент - счет
+    if "Счет" in number_card_or_account:
+        result = "Счет" + " " + masks.get_mask_account(int("".join(number_lst)))
+
+    # Используется эта маска, если принимаемый аргумент не счет
+    else:
+        result = "".join(letter_lst) + " " + masks.get_mask_card_number(int("".join(number_lst)))
+
+    return result
+
+
+print(mask_account_card("Счет 35383033474447895560"))
