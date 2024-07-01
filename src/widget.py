0fc89b3cfbcccba4df4f47be8dae3324ff797b6f
Egor Petruschenko <jaguar.94@mail.ru>
2024-07-01 12:09:46 +0600

    added a function showing the date

--- src/widget.py
+++ src/widget.py
@@ -24,3 +24,20 @@ def mask_account_card(number_card_or_account: str) -> str:
 
 
 print(mask_account_card("Счет 35383033474447895560"))
+
+
+def get_data(data_time: str) -> str:
+    """Функция возвращает дату"""
+    # Добавляем нужную нам дату в список с помощью срезов
+    date = []
+    date.append(data_time[8:10])
+    date.append(data_time[5:7])
+    date.append(data_time[:4])
+
+    # Переделываем список с датов в нужный нам формат
+    data = ".".join(date)
+
+    return data
+
+
+print(get_data("2018-07-11T02:26:18.671407"))
