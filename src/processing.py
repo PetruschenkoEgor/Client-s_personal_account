a7496cbb2b86a97b90d99624711ca26ff25e7358
Egor Petruschenko <jaguar.94@mail.ru>
2024-07-06 22:08:18 +0600

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -17,3 +17,14 @@ if __name__ == "__main__":
             ]
         )
     )
+
+def sort_by_date(list_dict, sort_order=True):
+    """Функция сортирует список словарей по дате"""
+    # Сортируем список словарей по ключу date
+    sorted_list = sorted(list_dict, key=lambda x:x["date"], reverse=sort_order)
+
+    return sorted_list
+
+
+if __name__ == "__main__":
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
