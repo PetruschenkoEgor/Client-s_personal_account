6c50dc68b37215723c668a78d79137f36b62d4c0
Egor Petruschenko <jaguar.94@mail.ru>
2024-07-23 17:47:30 +0600

    add test decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,4 +1,4 @@
-from src.decorators import my_function
+from src.decorators import my_function, log
 
 
 def test_log_ok(capsys):
@@ -6,3 +6,14 @@ def test_log_ok(capsys):
     my_function(1, 2)
     captured = capsys.readouterr()
     assert captured.out == "my_function ok\n"
+
+
+def test_log_division_by_zero(capsys):
+    """ Тест, если в функции выпадает исключение ZeroDivisionError """
+    @log()
+    def test_func_error(x, y):
+        return x / y
+
+    result = test_func_error(1, 0)
+    captured = capsys.readouterr()
+    assert "test_func_error error: division by zero. Inputs: {'x': 1, 'y': 0}\n" in captured.out
