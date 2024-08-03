0e8b762dcf461f14bb422f4644bc7b79c2865d18
Egor Petruschenko <jaguar.94@mail.ru>
2024-08-03 14:33:56 +0600

    editing

--- src/open_csv_and_excel.py
+++ src/open_csv_and_excel.py
@@ -1,17 +1,18 @@
 import csv
-import pandas as pd
 import os
 
+import pandas as pd
 
 PATH_TO_FILE_CSV = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions.csv")
 PATH_TO_FILE_EXCEL = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions_excel.xlsx")
 
-def get_transactions_from_csv(path_to_file: str) -> list[dict]:
-    """ Читает транзакции из CSV-файла """
+
+def get_transactions_from_csv(path_to_file: str) -> list[dict[str, int | str]]:
+    """Читает транзакции из CSV-файла"""
     try:
         # Открываем файл для чтения
-        with open(path_to_file, encoding='utf-8') as file_csv:
-            reader = csv.DictReader(file_csv, delimiter=';')
+        with open(path_to_file, encoding="utf-8") as file_csv:
+            reader = csv.DictReader(file_csv, delimiter=";")
             # Выдает список словарей с транзакциями
             transaction_list = [row for row in reader]
             # transactions = []
@@ -28,8 +29,9 @@ def get_transactions_from_csv(path_to_file: str) -> list[dict]:
 if __name__ == "__main__":
     print(get_transactions_from_csv(PATH_TO_FILE_CSV))
 
+
 def get_transactions_from_excel(path_to_file: str) -> list[dict]:
-    """ Читает транзакции из Excel-файла """
+    """Читает транзакции из Excel-файла"""
     try:
         # Считываем Excel-файл
         transactions_excel = pd.read_excel(path_to_file)
@@ -38,10 +40,10 @@ def get_transactions_from_excel(path_to_file: str) -> list[dict]:
         transactions_excel_not_nan = transactions_excel.loc[transactions_excel["from"].notnull()]
 
         # Преобразуем числа с плавающей точкой в int64
-        transactions_excel_not_nan = transactions_excel_not_nan.astype({"id": 'int64', "amount": 'int64'})
+        transactions_excel_not_nan = transactions_excel_not_nan.astype({"id": "int64", "amount": "int64"})
 
         # Преобразуем полученные данные в список словарей
-        transactions_list_dicts = transactions_excel_not_nan.to_dict(orient='records')
+        transactions_list_dicts = transactions_excel_not_nan.to_dict(orient="records")
 
         return transactions_list_dicts
     except FileNotFoundError:
--- tests/test_open_csv_and_excel.py
+++ tests/test_open_csv_and_excel.py
@@ -1,5 +1,5 @@
-from unittest.mock import patch
 import os
+from unittest.mock import patch
 
 from src.open_csv_and_excel import get_transactions_from_csv
 
@@ -9,7 +9,30 @@ PATH_TO_FILE_CSV = os.path.join(os.path.dirname(os.path.dirname(__file__)), "dat
 @patch("src.open_csv_and_excel.csv.DictReader")
 @patch("src.open_csv_and_excel.open")
 def test_get_transactions_from_csv(mock_open, mock_dict_reader, transactions_csv_excel):
-    """ Тест открытия файла с транзакциями CSV """
+    """Тест открытия файла с транзакциями CSV"""
     mock_dict_reader.return_value = transactions_csv_excel
-    assert get_transactions_from_csv(PATH_TO_FILE_CSV)[:2] == [{'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210', 'currency_name': 'Sol', 'currency_code': 'PEN', 'from': 'Счет 58803664561298323391', 'to': 'Счет 39745660563456619397', 'description': 'Перевод организации'}, {'id': '3598919', 'state': 'EXECUTED', 'date': '2020-12-06T23:00:58Z', 'amount': '29740', 'currency_name': 'Peso', 'currency_code': 'COP', 'from': 'Discover 3172601889670065', 'to': 'Discover 0720428384694643', 'description': 'Перевод с карты на карту'}]
-    mock_open.assert_called_once_with(PATH_TO_FILE_CSV, encoding='utf-8')
+    assert get_transactions_from_csv(PATH_TO_FILE_CSV)[:2] == [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        },
+        {
+            "id": "3598919",
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": "29740",
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        },
+    ]
+    mock_open.assert_called_once_with(PATH_TO_FILE_CSV, encoding="utf-8")
