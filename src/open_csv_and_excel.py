f7d0fd2cceacd50ed49d7f16a7a21f5c5afea461
Egor Petruschenko <jaguar.94@mail.ru>
2024-08-03 13:02:36 +0600

    editing functions

--- src/open_csv_and_excel.py
+++ src/open_csv_and_excel.py
@@ -6,25 +6,41 @@ import os
 PATH_TO_FILE_CSV = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions.csv")
 PATH_TO_FILE_EXCEL = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions_excel.xlsx")
 
-def get_transactions_from_csv(path_to_file: str) -> None:
+def get_transactions_from_csv(path_to_file: str) -> list[dict]:
     """ Читает транзакции из CSV-файла """
     # Открываем файл для чтения
     with open(path_to_file, encoding="utf_8") as file_csv:
         reader = csv.DictReader(file_csv, delimiter=';')
-        # Читаем файл построчно
-        for row in reader:
-            print(row)
+        # Выдает список словарей с транзакциями
+        transaction_list = [row for row in reader]
+        # transactions = []
+        # for d in transaction_list:
+        #     transactions.append({k: v for k, v in d.items() if v != ""})
+        # Удаляет ключи с пустыми значениями
+        transactions = [{k: v for k, v in d.items() if v != ""} for d in transaction_list]
+
+    return transactions
 
 
 if __name__ == "__main__":
-    get_transactions_from_csv(PATH_TO_FILE_CSV)
+    print(get_transactions_from_csv(PATH_TO_FILE_CSV))
 
-def get_transactions_from_excel(path_to_file: str) -> None:
+def get_transactions_from_excel(path_to_file: str) -> list[dict]:
     """ Читает транзакции из Excel-файла """
-    excel_data = pd.read_excel(path_to_file)
+    # Считываем Excel-файл
+    transactions_excel = pd.read_excel(path_to_file)
 
-    print(excel_data)
+    # Убираем значения NaN
+    transactions_excel_not_nan = transactions_excel.loc[transactions_excel["from"].notnull()]
+
+    # Преобразуем числа с плавающей точкой в int64
+    transactions_excel_not_nan = transactions_excel_not_nan.astype({"id": 'int64', "amount": 'int64'})
+
+    # Преобразуем полученные данные в список словарей
+    transactions_list_dicts = transactions_excel_not_nan.to_dict(orient='records')
+
+    return transactions_list_dicts
 
 
 if __name__ == "__main__":
-    get_transactions_from_excel(PATH_TO_FILE_EXCEL)
+    print(get_transactions_from_excel(PATH_TO_FILE_EXCEL))
